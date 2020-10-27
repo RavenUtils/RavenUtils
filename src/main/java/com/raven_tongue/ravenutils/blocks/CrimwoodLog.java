b02fa4d44b9eaa986b2dedb37a8893e9e684c389
Sascha Noschka <info@maskedia.com>
2020-10-27 17:02:31 +0100

    * clean-up

--- src/main/java/com/raven_tongue/ravenutils/blocks/CrimwoodLog.java
+++ src/main/java/com/raven_tongue/ravenutils/blocks/CrimwoodLog.java
@@ -1,6 +1,5 @@
 package com.raven_tongue.ravenutils.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
