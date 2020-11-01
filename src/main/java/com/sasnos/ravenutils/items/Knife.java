69a5d7a2c06e5568c9def57256185f8303a97d7c
Sascha Noschka <info@maskedia.com>
2020-11-01 02:49:40 +0100

    * added knives

---
+++ src/main/java/com/sasnos/ravenutils/items/Knife.java
@@ -0,0 +1,35 @@
+package com.sasnos.ravenutils.items;
+
+import com.sasnos.ravenutils.RavenUtils;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class Knife extends Item {
+  public Knife(int maxDamage) {
+    super(new Properties()
+        .group(RavenUtils.TAB)
+        .maxStackSize(1)
+        .maxDamage(maxDamage)
+        .setNoRepair());
+  }
+
+  public boolean doesContainerItemLeaveCraftingGrid(ItemStack stack) {
+    return false;
+  }
+
+  @Override
+  public boolean hasContainerItem(ItemStack stack) {
+    return true;
+  }
+
+  @Override
+  public ItemStack getContainerItem(ItemStack itemstack) {
+    ItemStack stack = itemstack.copy();
+    if (stack.getMaxDamage() - stack.getDamage() <= 1) {
+      stack.shrink(1);
+    } else {
+      stack.attemptDamageItem(1, random, null);
+    }
+    return stack;
+  }
+}
