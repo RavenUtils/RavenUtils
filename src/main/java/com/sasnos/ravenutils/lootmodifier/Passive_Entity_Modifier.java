7398bcd62c30a6b6651432083b4fa70030515219
SasNos <s.noschka@gmail.com>
2020-11-09 13:42:46 +0100

    * fixed minor variable confusion * minor formatting

--- src/main/java/com/sasnos/ravenutils/lootmodifier/Passive_Entity_Modifier.java
+++ src/main/java/com/sasnos/ravenutils/lootmodifier/Passive_Entity_Modifier.java
@@ -19,152 +19,156 @@ import java.util.List;
 
 public class Passive_Entity_Modifier extends LootModifier {
 
-    public int minHide;
-    public int maxHide;
-    public Item meat;
-    public int minMeat;
-    public int maxMeat;
-    public int minTallow;
-    public int maxTallow;
-    public int minBone;
-    public int maxBone;
-    public NonNullList<AdditionalItems> additional;
+  public int minHide;
+  public int maxHide;
+  public Item meat;
+  public int minMeat;
+  public int maxMeat;
+  public int minTallow;
+  public int maxTallow;
+  public int minBone;
+  public int maxBone;
+  public NonNullList<AdditionalItems> additional;
 
-    /**
-     * Constructs a LootModifier.
-     *
-     * @param conditionsIn the ILootConditions that need to be matched before the loot is modified.
-     */
-    public Passive_Entity_Modifier(
-            ILootCondition[] conditionsIn,
-            int minHideIn, int maxHideIn,
-            Item meatIn, int minMeatIn, int maxMeatIn,
-            int minTallowIn, int maxTallowIn,
-            int minBoneIn, int maxBoneIn,
-            NonNullList<AdditionalItems> additionalIN) {
-        super(conditionsIn);
-        this.minHide = minHideIn;
-        this.maxHide = maxHideIn;
-        this.meat = meatIn;
-        this.minMeat = minMeatIn;
-        this.maxMeat = maxMeatIn;
-        this.minTallow = minTallowIn;
-        this.maxTallow = maxTallowIn;
-        this.minBone = minBoneIn;
-        this.maxBone = maxBoneIn;
-        this.additional = additionalIN;
+  /**
+   * Constructs a LootModifier.
+   *
+   * @param conditionsIn the ILootConditions that need to be matched before the loot is modified.
+   */
+  public Passive_Entity_Modifier(
+      ILootCondition[] conditionsIn,
+      int minHideIn, int maxHideIn,
+      Item meatIn, int minMeatIn, int maxMeatIn,
+      int minTallowIn, int maxTallowIn,
+      int minBoneIn, int maxBoneIn,
+      NonNullList<AdditionalItems> additionalIN) {
+    super(conditionsIn);
+    this.minHide = minHideIn;
+    this.maxHide = maxHideIn;
+    this.meat = meatIn;
+    this.minMeat = minMeatIn;
+    this.maxMeat = maxMeatIn;
+    this.minTallow = minTallowIn;
+    this.maxTallow = maxTallowIn;
+    this.minBone = minBoneIn;
+    this.maxBone = maxBoneIn;
+    this.additional = additionalIN;
+  }
+
+  @Override
+  protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+
+    int looting = context.getLootingModifier();
+
+    if (maxHide > 0) {
+      int sum = context.getRandom().ints(minHide, maxHide).findFirst().getAsInt() + looting;
+      generatedLoot.add(new ItemStack(ModItems.FRESH_HIDE.get(), sum));
+    }
+
+    if (meat != null) {
+      int sum = context.getRandom().ints(minMeat, maxMeat).findFirst().getAsInt() + looting;
+      generatedLoot.add(new ItemStack(meat, sum));
+    }
+
+    if (maxTallow > 0) {
+      int sum = context.getRandom().ints(minTallow, maxTallow).findFirst().getAsInt() + looting;
+      generatedLoot.add(new ItemStack(ModItems.TALLOW.get(), sum));
+    }
+
+    if (maxBone > 0) {
+      int sum = context.getRandom().ints(minBone, maxBone).findFirst().getAsInt() + looting;
+      generatedLoot.add(new ItemStack(Items.BONE, sum));
+    }
+
+    for (AdditionalItems add : additional) {
+      int rand = context.getRandom().nextInt(100);
+      if (rand <= add.change) {
+        int sum = context.getRandom().ints(add.minItem, add.maxItem).findFirst().getAsInt();
+        if (add.useLootEnchant) sum += looting;
+        generatedLoot.add(new ItemStack(add.item, sum));
+      }
+    }
+
+    return generatedLoot;
+  }
+
+  public static class Serializer extends GlobalLootModifierSerializer<Passive_Entity_Modifier> {
+
+    @Override
+    public Passive_Entity_Modifier read(ResourceLocation location, JsonObject json, ILootCondition[] ailootcondition) {
+      int minHide = JSONUtils.getInt(json, "minHide");
+      int maxHide = JSONUtils.getInt(json, "maxHide");
+      int minTallow = JSONUtils.getInt(json, "minTallow");
+      int maxTallow = JSONUtils.getInt(json, "maxTallow");
+      int minMeat = JSONUtils.getInt(json, "minMeat");
+      int maxMeat = JSONUtils.getInt(json, "maxMeat");
+      Item meat = ForgeRegistries.ITEMS.getValue(new ResourceLocation(JSONUtils.getString(json, "meat")));
+      int minBone = JSONUtils.getInt(json, "minBone");
+      int maxBone = JSONUtils.getInt(json, "maxBone");
+      JsonArray additionalJson = JSONUtils.getJsonArray(json, "additional");
+      NonNullList<AdditionalItems> additional = NonNullList.create();
+
+      for (int i = 0; i < additionalJson.size() - 1; i++) {
+        JsonObject itemStack = additionalJson.get(i).getAsJsonObject();
+        additional.add(
+            new AdditionalItems(
+                ForgeRegistries.ITEMS.getValue(
+                    new ResourceLocation(
+                        JSONUtils.getString(itemStack, "item"))),
+                JSONUtils.getInt(itemStack, "minItem"),
+                JSONUtils.getInt(itemStack, "maxItem"),
+                JSONUtils.getInt(itemStack, "change"),
+                JSONUtils.getBoolean(itemStack, "useLooting")
+            )
+        );
+      }
+
+      return new Passive_Entity_Modifier(ailootcondition, minHide, maxHide, meat, minMeat, maxMeat, minTallow, maxTallow, minBone, maxBone, additional);
     }
 
     @Override
-    protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+    public JsonObject write(Passive_Entity_Modifier instance) {
+      JsonObject json = makeConditions(instance.conditions);
+      json.addProperty("minHide", instance.minHide);
+      json.addProperty("maxHide", instance.maxHide);
+      json.addProperty("minTallow", instance.minTallow);
+      json.addProperty("maxTallow", instance.maxTallow);
+      json.addProperty("minMeat", instance.minMeat);
+      json.addProperty("maxMeat", instance.maxMeat);
+      json.addProperty("meat", ForgeRegistries.ITEMS.getKey(instance.meat.getItem()).toString());
+      json.addProperty("minBone", instance.minBone);
+      json.addProperty("maxBone", instance.maxBone);
+      JsonArray additional = new JsonArray();
 
-        int looting = context.getLootingModifier();
+      for (AdditionalItems additionalItem : instance.additional) {
+        JsonObject itemstack = new JsonObject();
+        itemstack.addProperty("item", ForgeRegistries.ITEMS.getKey(additionalItem.item).toString());
+        itemstack.addProperty("minItem", additionalItem.minItem);
+        itemstack.addProperty("maxItem", additionalItem.maxItem);
+        itemstack.addProperty("change", additionalItem.change);
+        itemstack.addProperty("useLooting", additionalItem.useLootEnchant);
+        additional.add(itemstack);
+      }
+      json.add("additional", additional);
 
-        if(maxHide > 0){
-            int sum = context.getRandom().ints(minHide, maxHide).findFirst().getAsInt() + looting;
-            generatedLoot.add(new ItemStack(ModItems.FRESH_HIDE.get(), sum));
-        }
-
-        if(meat != null){
-            int sum = context.getRandom().ints(minMeat, maxMeat).findFirst().getAsInt() + looting;
-            generatedLoot.add(new ItemStack(meat, sum));
-        }
-
-        if(maxTallow > 0){
-            int sum = context.getRandom().ints(minTallow, maxTallow).findFirst().getAsInt() + looting;
-            generatedLoot.add(new ItemStack(ModItems.TALLOW.get(), sum));
-        }
-
-        if(maxBone > 0){
-            int sum = context.getRandom().ints(minBone, maxBone).findFirst().getAsInt() + looting;
-            generatedLoot.add(new ItemStack(Items.BONE, sum));
-        }
-
-        for(AdditionalItems add : additional){
-            int rand = context.getRandom().nextInt(100);
-            if(rand <= add.change){
-                int sum = context.getRandom().ints(add.minItem, add.maxItem).findFirst().getAsInt();
-                if(add.useLootEnchant) sum += looting;
-                generatedLoot.add(new ItemStack(add.item, sum));
-            }
-        }
-
-        return generatedLoot;
+      return json;
     }
 
-    public static class Serializer extends GlobalLootModifierSerializer<Passive_Entity_Modifier>{
+  }
 
-        @Override
-        public Passive_Entity_Modifier read(ResourceLocation location, JsonObject json, ILootCondition[] ailootcondition) {
-            int minHide = JSONUtils.getInt(json, "minHide");
-            int maxHide = JSONUtils.getInt(json, "maxHide");
-            int minTallow = JSONUtils.getInt(json, "minTallow");
-            int maxTallow = JSONUtils.getInt(json, "maxTallow");
-            int meatmin = JSONUtils.getInt(json,"maxMeat");
-            int meatMax = JSONUtils.getInt(json, "minMeat");
-            Item meat = ForgeRegistries.ITEMS.getValue(new ResourceLocation(JSONUtils.getString(json,"meat")));
-            int minBone = JSONUtils.getInt(json,"minBone");
-            int maxBone = JSONUtils.getInt(json,"maxBone");
-            JsonArray additionalJson = JSONUtils.getJsonArray(json, "additional");
-            NonNullList<AdditionalItems> additional = NonNullList.create();
-            for(int i = 0; i < additionalJson.size()-1;i++){
-                JsonObject itemStack = additionalJson.get(i).getAsJsonObject();
-                additional.add(
-                        new AdditionalItems(
-                                ForgeRegistries.ITEMS.getValue(
-                                        new ResourceLocation(
-                                                JSONUtils.getString(itemStack, "item"))),
-                                JSONUtils.getInt(itemStack, "minItem"),
-                                JSONUtils.getInt(itemStack, "maxItem"),
-                                JSONUtils.getInt(itemStack, "change"),
-                                JSONUtils.getBoolean(itemStack, "useLooting")
-                        )
-                );
-            }
+  public static class AdditionalItems {
+    public Item item;
+    public int minItem;
+    public int maxItem;
+    public int change;
+    public boolean useLootEnchant;
 
-            return new Passive_Entity_Modifier(ailootcondition, minHide, maxHide, meat, meatmin, meatMax, minTallow, maxTallow, minBone, maxBone, additional);
-        }
-
-        @Override
-        public JsonObject write(Passive_Entity_Modifier instance) {
-            JsonObject json = makeConditions(instance.conditions);
-            json.addProperty("minHide", instance.minHide);
-            json.addProperty("maxHide", instance.maxHide);
-            json.addProperty("minTallow", instance.minTallow);
-            json.addProperty("maxTallow", instance.maxTallow);
-            json.addProperty("minMeat", instance.minMeat);
-            json.addProperty("maxMeat", instance.maxMeat);
-            json.addProperty("meat", ForgeRegistries.ITEMS.getKey(instance.meat.getItem()).toString());
-            json.addProperty("minBone", instance.minBone);
-            json.addProperty("maxBone", instance.maxBone);
-            JsonArray additional = new JsonArray();
-            for(AdditionalItems additionalItem : instance.additional){
-                JsonObject itemstack = new JsonObject();
-                itemstack.addProperty("item", ForgeRegistries.ITEMS.getKey(additionalItem.item).toString());
-                itemstack.addProperty("minItem", additionalItem.minItem);
-                itemstack.addProperty("maxItem", additionalItem.maxItem);
-                itemstack.addProperty("change", additionalItem.change);
-                itemstack.addProperty("useLooting", additionalItem.useLootEnchant);
-                additional.add(itemstack);
-            }
-            json.add("additional", additional);
-            return json;
-        }
-
+    public AdditionalItems(Item itemIN, int minItemIn, int maxItemIn, int changeIn, boolean useLootEnchantIn) {
+      this.item = itemIN;
+      this.minItem = minItemIn;
+      this.maxItem = maxItemIn;
+      this.change = changeIn;
+      this.useLootEnchant = useLootEnchantIn;
     }
-    public static class AdditionalItems{
-        public Item item;
-        public int minItem;
-        public int maxItem;
-        public int change;
-        public boolean useLootEnchant;
-
-        public AdditionalItems(Item itemIN, int minItemIn, int maxItemIn, int changeIn, boolean useLootEnchantIn){
-            this.item = itemIN;
-            this.minItem = minItemIn;
-            this.maxItem = maxItemIn;
-            this.change = changeIn;
-            this.useLootEnchant = useLootEnchantIn;
-        }
-    }
+  }
 }
