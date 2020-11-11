e9d5862b0d7b073bb462877b15d2dc880cfc4143
Unbekannt <Unbekannt-github@protonmail.com>
2020-11-11 16:14:55 +0100

    First Loottable Provider with an example, also first example for GLM, GML also respects Burning now, also looting is handled with the correct function now

--- src/main/java/com/sasnos/ravenutils/lootModifier/PassiveEntityModifier.java
+++ src/main/java/com/sasnos/ravenutils/lootModifier/PassiveEntityModifier.java
@@ -3,11 +3,17 @@ package com.sasnos.ravenutils.lootModifier;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.sasnos.ravenutils.init.ModItems;
+import com.sun.istack.internal.NotNull;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameters;
+import net.minecraft.loot.RandomValueRange;
 import net.minecraft.loot.conditions.ILootCondition;
+import net.minecraft.loot.functions.LootingEnchantBonus;
+import net.minecraft.loot.functions.Smelt;
 import net.minecraft.util.JSONUtils;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
@@ -19,15 +25,15 @@ import java.util.List;
 
 public class PassiveEntityModifier extends LootModifier {
 
-  public int minHide;
-  public int maxHide;
+  public RandomValueRange hideDropRange;
+  public boolean applyLootingHide;
   public Item meat;
-  public int minMeat;
-  public int maxMeat;
-  public int minTallow;
-  public int maxTallow;
-  public int minBone;
-  public int maxBone;
+  public RandomValueRange meatDropRange;
+  public boolean applyLootingMeat;
+  public RandomValueRange tallowDropRange;
+  public boolean applyLootingTallow;
+  public RandomValueRange boneDropRange;
+  public boolean applyLootingBone;
   public NonNullList<AdditionalItems> additional;
 
   /**
@@ -37,21 +43,21 @@ public class PassiveEntityModifier extends LootModifier {
    */
   public PassiveEntityModifier(
       ILootCondition[] conditionsIn,
-      int minHideIn, int maxHideIn,
-      Item meatIn, int minMeatIn, int maxMeatIn,
-      int minTallowIn, int maxTallowIn,
-      int minBoneIn, int maxBoneIn,
+      RandomValueRange hideDropRangeIn, boolean applyLootHideIn,
+      Item meatIn, RandomValueRange meatDropRangeIn, boolean applyLootMeatIn,
+      RandomValueRange tallowDropRangeIn, boolean applyLootTallowIn,
+      RandomValueRange boneDropRangeIn, boolean applyLootBoneIn,
       NonNullList<AdditionalItems> additionalIN) {
     super(conditionsIn);
-    this.minHide = minHideIn;
-    this.maxHide = maxHideIn;
+    this.hideDropRange = hideDropRangeIn;
+    this.applyLootingHide = applyLootHideIn;
     this.meat = meatIn;
-    this.minMeat = minMeatIn;
-    this.maxMeat = maxMeatIn;
-    this.minTallow = minTallowIn;
-    this.maxTallow = maxTallowIn;
-    this.minBone = minBoneIn;
-    this.maxBone = maxBoneIn;
+    this.meatDropRange = meatDropRangeIn;
+    this.applyLootingMeat = applyLootMeatIn;
+    this.tallowDropRange = tallowDropRangeIn;
+    this.applyLootingTallow = applyLootTallowIn;
+    this.boneDropRange = boneDropRangeIn;
+    this.applyLootingBone = applyLootBoneIn;
     this.additional = additionalIN;
   }
 
@@ -59,31 +65,68 @@ public class PassiveEntityModifier extends LootModifier {
   protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
 
     int looting = context.getLootingModifier();
+    if (hideDropRange.getMax() > 0) {
+      ItemStack item;
+      if(applyLootingHide){
+        item = getItemStackWithLooting(context, hideDropRange, ModItems.FRESH_HIDE.get());
+      }
+      else{
+        item = new ItemStack(ModItems.FRESH_HIDE.get(), hideDropRange.generateInt(context.getRandom()));
+      }
 
-    if (maxHide > 0) {
-      int sum = context.getRandom().ints(minHide, maxHide).findFirst().getAsInt() + looting;
-      generatedLoot.add(new ItemStack(ModItems.FRESH_HIDE.get(), sum));
+      generatedLoot.add(item);
     }
 
     if (meat != null) {
-      int sum = context.getRandom().ints(minMeat, maxMeat).findFirst().getAsInt() + looting;
-      generatedLoot.add(new ItemStack(meat, sum));
+      ItemStack item;
+      Item contextMeat;
+      Entity entity = context.get(LootParameters.THIS_ENTITY);
+      if(entity.isBurning()){
+        Item tmpmeat = meat;
+        contextMeat = ((Smelt)Smelt.func_215953_b().build()).doApply(new ItemStack(meat), context).getItem();
+      }
+      else {
+        contextMeat = meat;
+      }
+      if(applyLootingMeat){
+       item = getItemStackWithLooting(context, meatDropRange, contextMeat);
+      }
+      else{
+        item = new ItemStack(contextMeat, meatDropRange.generateInt(context.getRandom()));
+      }
+      generatedLoot.add(item);
     }
 
-    if (maxTallow > 0) {
-      int sum = context.getRandom().ints(minTallow, maxTallow).findFirst().getAsInt() + looting;
-      generatedLoot.add(new ItemStack(ModItems.TALLOW.get(), sum));
+    if (tallowDropRange.getMax() > 0) {
+      ItemStack item;
+      if(applyLootingTallow){
+        item = getItemStackWithLooting(context, tallowDropRange, ModItems.TALLOW.get());
+      }
+      else
+      {
+        item = new ItemStack(ModItems.TALLOW.get(), tallowDropRange.generateInt(context.getRandom()));
+      }
+
+      generatedLoot.add(item);
     }
 
-    if (maxBone > 0) {
-      int sum = context.getRandom().ints(minBone, maxBone).findFirst().getAsInt() + looting;
-      generatedLoot.add(new ItemStack(Items.BONE, sum));
+    if (boneDropRange.getMax() > 0) {
+      ItemStack item;
+      if(applyLootingBone){
+        item = getItemStackWithLooting(context, boneDropRange, Items.BONE);
+      }
+      else
+      {
+        item = new ItemStack(Items.BONE, boneDropRange.generateInt(context.getRandom()));
+      }
+
+      generatedLoot.add(item);
     }
 
     for (AdditionalItems add : additional) {
-      int rand = context.getRandom().nextInt(100);
+      float rand = context.getRandom().nextFloat();
       if (rand <= add.change) {
-        int sum = context.getRandom().ints(add.minItem, add.maxItem).findFirst().getAsInt();
+        int sum = context.getRandom().ints(add.minItem, add.maxItem+1).findFirst().getAsInt();
         if (add.useLootEnchant) sum += looting;
         generatedLoot.add(new ItemStack(add.item, sum));
       }
@@ -92,21 +135,37 @@ public class PassiveEntityModifier extends LootModifier {
     return generatedLoot;
   }
 
+  @NotNull
+  private static ItemStack getItemStackWithLooting(LootContext context, RandomValueRange hideDropRange, Item loot) {
+    LootingEnchantBonus leb = (LootingEnchantBonus) new LootingEnchantBonus.Builder(hideDropRange).func_216072_a((int) hideDropRange.getMax()).build();
+    ItemStack item = leb.doApply(new ItemStack(loot), context);
+    return item;
+  }
+
   public static class Serializer extends GlobalLootModifierSerializer<PassiveEntityModifier> {
 
     @Override
     public PassiveEntityModifier read(ResourceLocation location, JsonObject json, ILootCondition[] ailootcondition) {
       int minHide = JSONUtils.getInt(json, "minHide");
       int maxHide = JSONUtils.getInt(json, "maxHide");
+      boolean applyLootHide = JSONUtils.getBoolean(json, "applyLootHide");
       int minTallow = JSONUtils.getInt(json, "minTallow");
       int maxTallow = JSONUtils.getInt(json, "maxTallow");
+      boolean applyLootTallow = JSONUtils.getBoolean(json, "applyLootTallow");
       int minMeat = JSONUtils.getInt(json, "minMeat");
       int maxMeat = JSONUtils.getInt(json, "maxMeat");
       Item meat = ForgeRegistries.ITEMS.getValue(new ResourceLocation(JSONUtils.getString(json, "meat")));
+      boolean applyLootMeat = JSONUtils.getBoolean(json, "applyLootMeat");
       int minBone = JSONUtils.getInt(json, "minBone");
       int maxBone = JSONUtils.getInt(json, "maxBone");
+      boolean applyLootBone = JSONUtils.getBoolean(json, "applyLootBone");
       JsonArray additionalJson = JSONUtils.getJsonArray(json, "additional");
       NonNullList<AdditionalItems> additional = NonNullList.create();
+
+      RandomValueRange hideRange = new RandomValueRange(minHide, maxHide);
+      RandomValueRange tallowRange = new RandomValueRange(minTallow, maxTallow);
+      RandomValueRange meatRange = new RandomValueRange(minMeat, maxMeat);
+      RandomValueRange boneRange = new RandomValueRange(minBone, maxBone);
 
       for (int i = 0; i < additionalJson.size() - 1; i++) {
         JsonObject itemStack = additionalJson.get(i).getAsJsonObject();
@@ -123,21 +182,32 @@ public class PassiveEntityModifier extends LootModifier {
         );
       }
 
-      return new PassiveEntityModifier(ailootcondition, minHide, maxHide, meat, minMeat, maxMeat, minTallow, maxTallow, minBone, maxBone, additional);
+      return new PassiveEntityModifier(
+              ailootcondition,
+              hideRange, applyLootHide,
+              meat, meatRange, applyLootMeat,
+              tallowRange, applyLootTallow,
+              boneRange, applyLootBone,
+              additional);
     }
 
     @Override
     public JsonObject write(PassiveEntityModifier instance) {
       JsonObject json = makeConditions(instance.conditions);
-      json.addProperty("minHide", instance.minHide);
-      json.addProperty("maxHide", instance.maxHide);
-      json.addProperty("minTallow", instance.minTallow);
-      json.addProperty("maxTallow", instance.maxTallow);
-      json.addProperty("minMeat", instance.minMeat);
-      json.addProperty("maxMeat", instance.maxMeat);
+
+      json.addProperty("minHide", instance.hideDropRange.getMin());
+      json.addProperty("maxHide", instance.hideDropRange.getMax());
+      json.addProperty("applyLootHide", instance.applyLootingHide);
+      json.addProperty("minTallow", instance.tallowDropRange.getMin());
+      json.addProperty("maxTallow", instance.tallowDropRange.getMax());
+      json.addProperty("applyLootTallow", instance.applyLootingTallow);
+      json.addProperty("minMeat", instance.meatDropRange.getMin());
+      json.addProperty("maxMeat", instance.meatDropRange.getMax());
       json.addProperty("meat", ForgeRegistries.ITEMS.getKey(instance.meat.getItem()).toString());
-      json.addProperty("minBone", instance.minBone);
-      json.addProperty("maxBone", instance.maxBone);
+      json.addProperty("applyLootMeat", instance.applyLootingMeat);
+      json.addProperty("minBone", instance.boneDropRange.getMin());
+      json.addProperty("maxBone", instance.boneDropRange.getMax());
+      json.addProperty("applyLootBone", instance.applyLootingBone);
       JsonArray additional = new JsonArray();
 
       for (AdditionalItems additionalItem : instance.additional) {
@@ -160,10 +230,10 @@ public class PassiveEntityModifier extends LootModifier {
     public Item item;
     public int minItem;
     public int maxItem;
-    public int change;
+    public float change;
     public boolean useLootEnchant;
 
-    public AdditionalItems(Item itemIN, int minItemIn, int maxItemIn, int changeIn, boolean useLootEnchantIn) {
+    public AdditionalItems(Item itemIN, int minItemIn, int maxItemIn, float changeIn, boolean useLootEnchantIn) {
       this.item = itemIN;
       this.minItem = minItemIn;
       this.maxItem = maxItemIn;
