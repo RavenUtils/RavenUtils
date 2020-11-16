25435ee8283c1c7e45b8baca74ab1b437ac1c97a
SasNos <s.noschka@gmail.com>
2020-11-16 16:25:34 +0100

    * added Duck Egg placeholder

--- src/main/java/com/sasnos/ravenutils/init/ModFoodItems.java
+++ src/main/java/com/sasnos/ravenutils/init/ModFoodItems.java
@@ -11,11 +11,10 @@ public class ModFoodItems {
 
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RavenUtils.MOD_ID);
 
-  // meat
   public static final RegistryObject<Item> BUSHMEAT = ITEMS.register("bushmeat", Bushmeat::new);
   public static final RegistryObject<Item> COOKED_BUSHMEAT = ITEMS.register("cooked_bushmeat", CookedBushmeat::new);
 
   public static final RegistryObject<Item> FLOUR = ITEMS.register("flour", ItemBase::new);
   public static final RegistryObject<Item> DOUGH = ITEMS.register("dough", ItemBase::new);
-  public static final RegistryObject<Item> DUCK_EGG = ITEMS.register("duck_egg", ItemBase::new);
+  public static final RegistryObject<Item> DUCK_EGG = ITEMS.register("duck_egg", DuckEgg::new);
 }
---
+++ src/main/java/com/sasnos/ravenutils/items/DuckEgg.java
@@ -0,0 +1,12 @@
+package com.sasnos.ravenutils.items;
+
+import com.sasnos.ravenutils.RavenUtils;
+import net.minecraft.item.BoneMealItem;
+import net.minecraft.item.EggItem;
+
+public class DuckEgg extends EggItem {
+  public DuckEgg() {
+    super(new Properties()
+        .group(RavenUtils.TAB));
+  }
+}
