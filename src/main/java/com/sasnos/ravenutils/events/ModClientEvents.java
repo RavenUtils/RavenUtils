2acf18da451680d5e9e3cc04f2585b72efd7a03b
Unbekannt <Unbekannt-github@protonmail.com>
2020-12-10 00:01:08 +0100

    added the Lootable for the fossile Dirt, added way to optain flint shard, added Sticks and Stones to the world

--- src/main/java/com/sasnos/ravenutils/events/ModClientEvents.java
+++ src/main/java/com/sasnos/ravenutils/events/ModClientEvents.java
@@ -1,18 +1,35 @@
 package com.sasnos.ravenutils.events;
 
 import com.sasnos.ravenutils.RavenUtils;
+import com.sasnos.ravenutils.init.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = RavenUtils.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ModClientEvents {
 
-  /* todo implement getting Flitn Shards from FLint on Overworld Stone
+  // todo implement getting Flitn Shards from FLint on Overworld Stone
   @SubscribeEvent
-  public static void smackFlintIntoShards(PlayerInteractEvent useFlint) {
-    PlayerEntity player = useFlint.getPlayer();
-    Item item = player.getHeldItemMainhand().getItem();
+  public static void smackFlintIntoShards(PlayerInteractEvent.RightClickBlock useFlint) {
+        World world = useFlint.getWorld();
+        if(world.isRemote) return;
+        Block block = world.getBlockState(useFlint.getPos()).getBlock();
+
+        if(!(block == Blocks.STONE || block == Blocks.OBSIDIAN)) return;
+        PlayerEntity player = useFlint.getPlayer();
+        ItemStack item = player.getHeldItem(useFlint.getHand());
+        if(item.getItem() == Items.FLINT){
+            item.shrink(1);
+            player.addItemStackToInventory(new ItemStack(ModItems.FLINT_SHARD.get(), 2));
+        }
   }
-  */
 
 }
